8c9ff4838f379e0576c641149c3aa3ded982d507
Volodymyr Vysochanskyi <v.vysochanskyi@svitla.com>
2017-04-28 22:09:15 +0300

    rallback routes file

--- view/frontend/web/js/recommend-variables.js
+++ view/frontend/web/js/recommend-variables.js
@@ -4,52 +4,8 @@ define(['uiComponent', 'Magento_Customer/js/customer-data', 'ko'], function(Comp
         variables: {},
 
         initialize: function () {
-            //var self = this,
-            //koexampleData = customerData.get('koexample');
-            //console.log(koexampleData());
-            //this.update(koexampleData());
-
             this._super();
-            //var d = new Date();
-            //this.sayHello = "Hello this is content populated with KO! " + d.getTime();
             this.variables = customerData.get('recommend');
         }
-        //,
-        //isLoading: ko.observable(false),
-        /**
-         * Update mini shopping cart content.
-         *
-         * @param {Object} updatedCart
-         * @returns void
-         */
-        //update: function (recommendData) {
-        //    console.log('update');
-        //    _.each(recommendData, function (value, key) {
-        //        console.log(value);
-        //        console.log(key);
-        //        if (!this.variables.hasOwnProperty(key)) {
-        //            this.variables[key] = ko.observable();
-        //        }
-        //        this.variables[key](value);
-        //    }, this);
-        //},
-
-        /**
-         * Get cart param by name.
-         * @param {String} name
-         * @returns {*}
-         */
-        //getVariable: function (name) {
-        //    console.log('getVariable');
-        //    console.log(name);
-        //    if (!_.isUndefined(name)) {
-        //        if (!this.variables.hasOwnProperty(name)) {
-        //            this.variables[name] = ko.observable();
-        //        }
-        //    }
-        //
-        //    return this.variables[name]();
-        //}
-
     });
 });
